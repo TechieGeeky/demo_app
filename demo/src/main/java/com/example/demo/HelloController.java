2de8977185256dc30428594dc83c97292b806ea9
ashok <ashok@ASHOK>
2026-09-24 15:46:50 +0530

    hello controller

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public ResponseEntity<?> hello(){
+		
+		
+		return new ResponseEntity<>("Hello", HttpStatus.OK);
+	}
+
+}
